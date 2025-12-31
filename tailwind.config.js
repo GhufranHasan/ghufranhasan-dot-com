7393b78fec708455efc2ccaddd9fd1404fe234e2
Ghufran Hasan <ghufran.aims@gmail.com>
2025-12-31 10:50:59 +0500

    Reinstalling and setup the whole project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        purple: { dark: '#270320', accent: '#560746' },
+        'neon-pink': '#310e3d',
+        orange: { primary: '#fd8303', hover: '#fb8106' },
+      },
+    },
+  },
+};
